b697646ff2e155d7c58596de49c5384ff0c0e7d3
LeopoldV <varlet.leopold@gmail.com>
2026-03-09 16:23:38 +0100

    ADD dockerfile and docker image doc

--- src/model.py
+++ src/model.py
@@ -8,7 +8,7 @@ class ChestXRayModel(nn.Module, PyTorchModelHubMixin,repo_url="chestxpert",
     license="mit",):
     def __init__(self, num_classes=14):
         super(ChestXRayModel, self).__init__()
-        self.backbone = torchvision.models.efficientnet_b0(weights = torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1)
+        self.backbone = torchvision.models.efficientnet_b0()
         orig_conv = self.backbone.features[0][0]
         self.backbone.features[0][0] = nn.Conv2d(1, orig_conv.out_channels,
                         kernel_size=orig_conv.kernel_size,
